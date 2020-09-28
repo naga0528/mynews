dea8bb205289384efa035f7b5782e09212a5e653
nagashima taiki <nagashi030528@gmail.com>
2020-09-28 11:45:08 +0000

    profile

---
+++ resources/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charet="utf-8">
+        <meta http-equiv="X-UA-Compatibie" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Profile create</title>
+    </head>
+    <body>
+        <h1>プロフィール作成画面</h1>
+    </body>
+</html>
