740fd51cd46ae1ca3dff1e34c6b3372906a32267
nagashima taiki <nagashi030528@gmail.com>
2020-10-26 11:54:37 +0000

    16 投稿したニュースを更新/削除しよう 課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,40 +2,94 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\profile;
-
+//Profile Modelが使えるよになる
+use App\Profile;
+use App\Profilehistory;
+use Carbon\Carbon;
 class ProfileController extends Controller
 {
+    //
     public function add()
     {
         return view('admin.profile.create');
     }
-
-    public function create()
+    
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        //データベースに保存
+        $profile->fill($form);
+        $profile->save();  //保存はsaveメソッドを使うだけ
+        //リダイレクト
         return redirect('admin/profile/create');
     }
-
-    public function edit()
+    
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-
-    public function update()
+    
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
-    }
-    public function profile(Request $request)
-    {
+      // Validationをかける
       $this->validate($request, Profile::$rules);
-      $profile = new Profile;
-      $form = $request->all();
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      if ($request->remove == 'true') {
+            $profile_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $profile_form['image_path'] = basename($path);
+        } else {
+            $profile_form['image_path'] = $profile->image_path;
+        }
+      unset($profile_form['_token']);
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
       
-      unset($form['_token']);
-      
-      $profile->fill($form);
-      $profile->save();
-      return redirect('admin/profile/create');
-  }
-}
+     // 以下を17で追記
+      $history = new Profilehistory;
+      $history->profile_id = $profile->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
+      return redirect('admin/profile');
+    }
 
+     public function delete(Request $request)
+    {
+      // 該当するNews Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+    }  
+
+    public function index(Request $request)
+    {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する  
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          //$cond_title（ユーザーが入力した文字）で一致しない全てのレコードを取得する
+          $posts = Profile::all();
+      }
+      //index.blade.phpのファイルに取得したレコード（$posts）と
+      //      ユーザーが入力した文字列（$cond_title）を渡し、ページを開く
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,18 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('profile/edit','Admin\ProfileController@edit');
     Route::post('profile/edit','Admin\ProfileController@update');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
